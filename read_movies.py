ac6d8b8a339d5e5a5b9ec4de5ff64387825b9502
kaitlynstaut <kaitlyn.staut@drake.edu>
2026-02-28 13:46:31 -0600

    add runtime to print_movies function

--- read_movies.py
+++ read_movies.py
@@ -22,6 +22,7 @@ def print_movie(movie):
     title = movie.get("Title", "Unknown Title")
     year = movie.get("Year", "Unknown Year")
     ratings = movie.get("Ratings", "No ratings")
+    runtime = movie.get("Runtime", "Unkown runtime")
 
     print(f"  Title  : {title}")
     print(f"  Year   : {year}")
